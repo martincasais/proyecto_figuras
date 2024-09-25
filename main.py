2c8cc5b104f7612bbc17b4eae2ab7a4c822fc337
Martín Casais Blanco <martin.casais@rai.usc.es>
2024-09-25 12:38:41 +0200

    Agregadas nuevas funciones al main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from lib import cuadrado
 print("Proyecto Figuras")
+print(cuadrado.get_identificador())
 lado=4
-print(f"El área de un cuadrado de lado {lado} es: {cuadrado.get_area(lado)}")
+print(f"El área de un {cuadrado.get_identificador()} de lado {lado} es: {cuadrado.get_area(lado)} y el perímetro es {cuadrado.get_perimetro(lado)}")
